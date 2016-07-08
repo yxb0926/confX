631cba470c2fe41b24aca6bdc44a3862f62b68ae
yxb <yxb0926@163.com>
2016-07-08 17:16:52 +0800

    xx

--- src/main/java/com/nice/confX/controller/PreviewController.java
+++ src/main/java/com/nice/confX/controller/PreviewController.java
@@ -1,9 +1,13 @@
 package com.nice.confX.controller;
 
+import com.nice.confX.service.manager.MySQLService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by yxb on 16/7/7.
@@ -12,6 +16,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/preview")
 public class PreviewController {
+
+    @Autowired
+    private MySQLService mySQLService;
+
 
     @RequestMapping("/index")
     public ModelAndView index(){
@@ -23,20 +31,22 @@ public class PreviewController {
 
     @RequestMapping("/MySQL/index")
     @ResponseBody
-    public String mysqlIndex(String pcode, String groupid){
+    public List mysqlIndex(String pcode, String groupid){
 
+        List myList = null;
         if ( pcode == null || pcode.length() <=0 ){
             // 如果不指定pcode,则直接返回null
             return null;
         }else if (groupid == null || groupid.length() <=0 ){
             // 获取整个pcode的数据
+            myList = mySQLService.getAllMyConf(pcode);
         }else {
             // 获取pcode下的groupid的数据,
             // 注意,groupid可以是一串id|id|id,可以获取单个或多个groupid
-
+            myList = mySQLService.getOneMyConf(pcode, groupid);
         }
 
 
-        return "xxxxxxxxxxxxxx";
+        return myList;
     }
 }
